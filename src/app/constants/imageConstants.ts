5c2e9747fd832e439d2f531a05bc3df23849ce3f
Pedro Agustin Vega <vegapedroagustin@gmail.com>
2024-05-09 17:22:01 -0300

    MCPTool Icon image added

---
+++ src/app/constants/imageConstants.ts
@@ -0,0 +1,5 @@
+import mcptoolServerCommand from "@public/mcptool-server-command.png";
+import mcptoolIcon from "@public/icon.ico"
+
+export const MCPTOOL_SERVER = mcptoolServerCommand.src; 
+export const MCPTOOL_ICON = mcptoolIcon.src;
